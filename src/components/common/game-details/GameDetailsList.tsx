cced2f01e0d95f91381abac013aaf1148d074820
Christian Jucutan <cjucutan@rrc.ca>
2025-09-18 12:41:30 -0500

    Fixed the look of the list

--- src/components/common/game-details/GameDetailsList.tsx
+++ src/components/common/game-details/GameDetailsList.tsx
@@ -2,16 +2,19 @@ import games from "../../data/games.json";
 
 export function GameList() {
   return (
-    <section>
-      {games.map((game) => (
-        <div>
-          {Object.entries(game).filter(([key]) => key !== "id").map(([detail, info]) => (
-            <p key={detail}>
-              <span>{detail}: {info}</span>
-            </p>
-          ))}
-        </div>
-      ))}
+    <section className="place-content-center p-10 grid ">
+      <div className="max-w-md break-words">
+        {games.map((game) => (
+          <div className="text-center rounded-2xl border bg-white p-4 bg-linear-to-br from-sky-800 via-blue-900 to-indigo-950 text-white">
+            {Object.entries(game).filter(([key]) => key !== "id").map(([detail, info]) => (
+              <p key={detail}>
+                <h4 className="p-6 underline font-bold ">{detail}</h4>
+                <span>{info}</span>
+              </p>
+            ))}
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
