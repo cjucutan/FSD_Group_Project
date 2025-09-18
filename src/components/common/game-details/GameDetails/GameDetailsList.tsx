f10a99acf5d49c95610d830e17a66eab4d48fca9
Christian Jucutan <cjucutan@rrc.ca>
2025-09-17 23:46:15 -0500

    Restructured the file directory

---
+++ src/components/common/game-details/GameDetails/GameDetailsList.tsx
@@ -0,0 +1,17 @@
+import games from "../../../data/games.json";
+
+export function GameList() {
+  return (
+    <section>
+      {games.map((game) => (
+        <div>
+          {Object.entries(game).filter(([key]) => key !== "id").map(([detail, info]) => (
+            <p key={detail}>
+              <span>{detail}: {info}</span>
+            </p>
+          ))}
+        </div>
+      ))}
+    </section>
+  );
+}
