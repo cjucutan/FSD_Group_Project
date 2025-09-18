9a5a5c9522873e561ef3aa7e6857a34d8b06b363
Christian Jucutan <cjucutan@rrc.ca>
2025-09-18 13:33:15 -0500

    Feat: Created the Navbar that goes to each individual component

---
+++ src/components/common/Header/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header>
+            <h1>Cartridge Cache</h1>
+        </header>
+    )
+};
+
+export default Header;
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,8 +1,10 @@
 import { GameList } from "../common/game-details/GameDetailsList"
+import Nav  from "../common/navbar/Nav"
 
 export function HomePage() {
     return (
         <>
+            <Nav />
             <GameList /> 
         </>
     )
