6ec684e0ea7e343f9e4578209323df33d2a8e0c1
Dominic Janaban <djanaban@academic.rrc.ca>
2025-10-05 03:32:48 -0500

    feat: add styling for profile page (might change button if it causes problems with the form later on)

--- src/components/common/user-profile/profile-page.tsx
+++ src/components/common/user-profile/profile-page.tsx
@@ -27,12 +27,23 @@ export function Profile(){
     return(
         <div >
             <h1 className="flex justify-center text-4xl mb-4">My Profile</h1>
-            <p className="flex justify-center my-4">USERNAME: {user.username}</p>
-            <p className="flex justify-center my-4">EMAIL: {user.email}</p>
-            <p className="flex justify-center my-4">AVATARURL: {user.avatarUrl}</p>
-            <p className="flex justify-center my-4">BIO: {user.bio}</p>
+            <div className="flex flex-col justify-center rounded-2xl border bg-white p-4 bg-linear-to-br 
+                            from-sky-500 via-blue-900 to-indigo-950 text-white max-w-md mx-auto">
+                <p className="flex justify-center my-4">USERNAME: {user.username}</p>
+                <p className="flex justify-center my-4">EMAIL: {user.email}</p>
+                <p className="flex justify-center my-4">AVATARURL: {user.avatarUrl}</p>
+                <p className="flex justify-center my-4">BIO: {user.bio}</p>
+            </div>
             <div className="flex justify-center">
-                <button onClick={() => handleUpdate()} >Update Profile</button>
+                <button className="flex justify-center align-center my-4 h-10 w-32 rounded-2xl 
+                                    bg-linear-to-br from-sky-950 via-blue-900 to-indigo-500 text-white
+                                    hover:bg-linear-blue-900"
+                                    onClick={() => handleUpdate()}>
+                                        <div className="flex h-full w-full items-center justify-center 
+                                        bg-gray-900 rounded-full hover:bg-blue-500">
+                                            Update Profile
+                                        </div>
+                </button>
             </div>
 
         </div>
