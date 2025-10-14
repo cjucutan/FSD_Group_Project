2f9c86a25d0a8293c179d8e8ab84f7006bd638ab
Patrick Luke Dela Torre <pdelatorre@academic.rrc.ca>
2025-10-14 14:14:18 -0500

    Added the ts file to replace the json file and added some types to connect to other components.

--- src/components/common/games/games.tsx
+++ src/components/common/games/games.tsx
@@ -1,11 +1,11 @@
-import games from '../../data/games.json';
+import { games } from '../../data/gamesList';
 import gameImage from '../../data/images/games.jpg';
 
 export default function AllGames() {
     return (
         <section>
-            {games.slice(0, 3).map(({ id, gameName }) => (
-                <div key={id} className="flex items-center justify-space-evenly flex-col m-4s">
+            {games.slice(0, 5).map(({ id, gameName }) => (
+                <div key={id} className="flex items-center justify-space-around flex-col m-4s">
                     <img src={gameImage} height= "200" width="300" alt="Game cover" className="game-image" />
                     <h3 className="text-center font-bold text-lg">{gameName}</h3>
                 </div>
